153df7a3f7ace89cb3887d046a3e330721688b61
lilheep <andrey.arkhipov.2006@mail.ru>
2025-06-18 21:38:24 +0300

    update 3

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 from email_utils import send_email, generation_confirmation_code
 from datetime import datetime, timedelta
 import uuid
+from typing import Optional
 
 
 app = FastAPI()
@@ -32,8 +33,22 @@ class AuthRequest(BaseModel):
     email: str | None = None
     number_phone: str | None = None
     password: str
+    
+class TourSchema(BaseModel):
+    name: str
+    description: str | None = None
+    price: int
+    days: int
+    country: str  
 
-@app.post('/users/register/')
+class TourSchemaUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[int] = None
+    days: Optional[int] = None
+    country: Optional[str] = None
+  
+@app.post('/users/register/', tags=['Users'])
 async def create_user(email: str, password: str, full_name: str, number_phone: str):
     if not re.fullmatch(EMAIL_REGEX, email) or not re.fullmatch(PHONE_REGEX, number_phone):
         raise HTTPException(400, 'Неверный формат данных email/номера телефона.')
@@ -54,7 +69,7 @@ async def create_user(email: str, password: str, full_name: str, number_phone: s
     except Exception as e:
         raise HTTPException(500, f'Произошла ошибка при регистрации: {e}')
             
-@app.post('/users/auth/')
+@app.post('/users/auth/', tags=['Users'])
 async def auth_user(data: AuthRequest):
     email = data.email
     number_phone = data.number_phone
@@ -97,7 +112,7 @@ async def auth_user(data: AuthRequest):
     except Exception as e:
         raise HTTPException(500, f'Произошла ошибка при авторизации: {e}')
 
-@app.post('/users/change_password/')
+@app.post('/users/change_password/', tags=['Users'])
 async def request_password_change(email: str):
     user = Users.select().where(Users.email==email).first()
     if not user:
@@ -116,7 +131,7 @@ async def request_password_change(email: str):
     
     return {'message': 'Код подтверждения успешно отправлен.'}
 
-@app.post('/users/confirm_change_password/')
+@app.post('/users/confirm_change_password/', tags=['Users'])
 async def confirm_password_change(email: str, code: str, new_password: str):
     user = Users.select().where(Users.email==email).first()
     if not user:
@@ -141,7 +156,7 @@ async def confirm_password_change(email: str, code: str, new_password: str):
     
     return {'message': 'Пароль успешно обновлен.'}
 
-@app.delete('/users/delete_profile/')
+@app.delete('/users/delete_profile/', tags=['Users'])
 async def delete_profile(token: str = Form(...)):
     user = Users.select().where(Users.token == token).first()
     if not user:
@@ -149,7 +164,7 @@ async def delete_profile(token: str = Form(...)):
     user.delete_instance()
     return {'message': 'Аккаунт успешно удален.'}
 
-@app.get('/users/me/')
+@app.get('/users/me/', tags=['Users'])
 async def get_profile(token: str):
     user = Users.select().where(Users.token == token).first()
     if not user:
@@ -160,3 +175,79 @@ async def get_profile(token: str):
         'full_name': user.full_name,
         'number_phone': user.number_phone
     }
+
+@app.post('/tours/create/', tags=['Tours'])
+async def create_tour(data: TourSchema):
+    name = data.name
+    description = data.description
+    price = data.price
+    days = data.days
+    country = data.country
+    
+    try:
+        Tours.create(name=name, description=description, price=price, days=days, country=country)
+        return {'message': 'Тур успешно создан.'}
+    except Exception as e:
+        raise HTTPException(500, f'Ошибка при создании тура: {e}')
+
+@app.get('/tours/get_tours/', tags=['Tours'])
+async def get_all_tours():
+    tours = Tours.select()
+    return [{
+        'Название тура:': t.name,
+        'Описание:': t.description,
+        'Цена': t.price,
+        'Длительность': t.days,
+        'Страна': t.country
+    } for t in tours
+    ]
+    
+@app.get('/tours/get_tour_id', tags=['Tours'])
+async def get_tour_by_id(tour_id: int):
+    tour = Tours.select().where(Tours.id==tour_id).first()
+    if not tour:
+        raise HTTPException(404, 'Указанный тур не найден.')
+    try:
+        return {
+            'Название тура:': tour.name,
+            'Описание:': tour.description,
+            'Цена': tour.price,
+            'Длительность': tour.days,
+            'Страна': tour.country
+        }
+    except Exception as e:
+        raise HTTPException(500, f'Ошибка при получении тура: {e}')
+
+@app.patch('/tours/update/', tags=['Tours'])
+async def update_tour(tour_id: int, data: TourSchemaUpdate):
+    tour = Tours.select().where(Tours.id==tour_id).first()
+    if not tour:
+        raise HTTPException(404, 'Указанный тур не найден.')
+    try:
+        if data.name is not None:
+            tour.name = data.name
+        if data.description is not None:
+            tour.description = data.description
+        if data.price is not None:
+            tour.price = data.price
+        if data.days is not None:
+            tour.days = data.days
+        if data.country is not None:
+            tour.country = data.country
+        
+        tour.save()
+        return {'message': 'Информация о туре успешно изменена.'}
+    except Exception as e:
+        raise HTTPException(500, f'Ошибка при обновлении данных о туре: {e}.')
+
+@app.delete('/tours/delete_tour/', tags=['Tours'])
+async def delete_tour_by_id(tour_id: int):
+    tour = Tours.select().where(Tours.id==tour_id).first()
+    if not tour:
+        raise HTTPException(404, 'Указанный тур не найден.')
+    
+    try:
+        tour.delete_instance()
+        return {'message': 'Тур успешно удален.'}
+    except Exception as e:
+        raise HTTPException(500, f'Ошибка, тур не был удален: {e}')
